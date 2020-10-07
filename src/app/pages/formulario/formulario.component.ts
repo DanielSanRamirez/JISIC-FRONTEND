421b988a47b828e641712764bac3753b0909b1ce
DanielSanRamirez <danielsanramirez@hotmail.com>
2020-10-07 00:08:34 -0500

    Quitar valores por defecto

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -86,13 +86,13 @@ export class FormularioComponent implements OnInit, AfterViewInit {
     );
 
     this.participanteForm = this._fb.group({
-      nombres: ['d ', [Validators.required, Validators.pattern('[a-zA-ZÀ-ÿ\u00f1\u00d1 ]*')]],
-      apellidos: ['d', [Validators.required, Validators.pattern('[a-zA-ZÀ-ÿ\u00f1\u00d1 ]*')]],
-      direccion: ['d', Validators.required],
-      codTelefono: ['5ec46b09c0bb9535c0ebdb87', Validators.required],
-      telefono: ['3', [Validators.required, Validators.pattern('[0-9]*')]],
-      email: ['d@d', [Validators.required, Validators.email]],
-      pais: ['5ec46b09c0bb9535c0ebdb87', Validators.required]
+      nombres: ['', [Validators.required, Validators.pattern('[a-zA-ZÀ-ÿ\u00f1\u00d1 ]*')]],
+      apellidos: ['', [Validators.required, Validators.pattern('[a-zA-ZÀ-ÿ\u00f1\u00d1 ]*')]],
+      direccion: ['', Validators.required],
+      codTelefono: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      email: ['', [Validators.required, Validators.email]],
+      pais: ['', Validators.required]
     })
     
   }
